d712124ba5fde45806fe93989df3d5162ebd014a
Rajvi2 <122998987+Rajvi2@users.noreply.github.com>
2023-03-22 12:11:29 -0400

    Angles Display

--- PolygonChecker/triangleSolver.c
+++ PolygonChecker/triangleSolver.c
@@ -4,7 +4,8 @@
 
 #include "triangleSolver.h"
 
-char* analyzeTriangle(float side1, float side2, float side3) {
+char* analyzeTriangle(float side1, float side2, float side3)
+{
 	char* result = "";
 	if (side1 <= 0 || side2 <= 0 || side3 <= 0) {
 		result = "Not a triangle";
@@ -12,14 +13,15 @@ char* analyzeTriangle(float side1, float side2, float side3) {
 	else if (side1 == side2 && side1 == side3) {
 		result = "Equilateral triangle";
 	}
-	else if ((side1 == side2 && side1 != side3) || 
-		(side1 == side3 && side1 != side2))
-	{
+	else if ((side1 == side2 && side1 != side3) ||
+	(side1 == side3 && side1 != side2)){
 		result = "Isosceles triangle";
 	}
 	else {
 		result = "Scalene triangle";
-	}	
+	};
+
+
 	float angleA, angleB, angleC, R, s, pi, area;
 	pi = acos(-1);
 
@@ -32,18 +34,28 @@ char* analyzeTriangle(float side1, float side2, float side3) {
 	angleB = (180 / pi) * asin(side2 / 2 * R);
 	angleC = (180 / pi) * asin(side3 / 2 * R);
 
-	printf("%.2f %.2f %.2f", angleA, angleB, angleC);
+	//printf("%.2f %.2f %.2f", angleA, angleB, angleC);
+
+	if (side1 <= 0 && side2 <= 0 && side3 <= 0)
+	{
+		printf("The given sides does not create a triangle\n");
+
+
+	}
+	else if (side1 + side2 <= side3 || side1 + side3 <= side2 || side2 + side3 <= side1)
+	{
+		printf("The given sides does not create a triangle\n");
+
+	}
+	else if (side1 + side2 > side3 || side2 + side3 > side1 || side1 + side3 > side2)
+	{
+
+		printf("Sides:  %d %d %d\n", side1, side2, side3);
+		printf("Angles: %.2f %.2f %.2f\n", angleA, angleB, angleC);
+	}
 	return result;
-}/*
-void angle(float angleA, float angleB, float angleC, float pi, float R)
-{
+}
 
-	angleA = (180 / pi) * asin(side1 / (2 * R));
-	angleB = (180 / pi) * asin(side2 / (2 * R));
-	angleC = (180 / pi) * asin(side3 / (2 * R));
 
-	printf("The sides are: %.2f\t   %.2f\t     %.2f\t\n", side1, side2, side3);
-	printf("The angles are: %.2f\t%.2f\t%.2f\n", angleA, angleB, angleC);
 
-}*/
 
--- PolygonChecker/triangleSolver.h
+++ PolygonChecker/triangleSolver.h
@@ -1,2 +1,3 @@
 #pragma once
 char* analyzeTriangle(side1,side2, side3);
+int* analyzeAngles(int angleA, int angleB, int angleC);
