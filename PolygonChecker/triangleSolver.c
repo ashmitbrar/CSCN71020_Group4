e1a101fce10843ff6ac3c3f0f265a9e94482850c
Yeshd <yeshodaprasai@gmail.com>
2023-03-30 10:05:30 -0400

    Try Rectangle part

--- PolygonChecker/triangleSolver.c
+++ PolygonChecker/triangleSolver.c
@@ -65,3 +65,20 @@ char* analyzeTriangle(int side1, int side2, int side3) {
 
 
 
+bool isRectangle(double x1, double y1,
+	double x2, double y2,
+	double x3, double y3,
+	double x4, double y4)
+{
+	double cx, cy;
+	double dd1, dd2, dd3, dd4;
+
+	cx = (x1 + x2 + x3 + x4) / 4;
+	cy = (y1 + y2 + y3 + y4) / 4;
+
+	dd1 = sqr(cx - x1) + sqr(cy - y1);
+	dd2 = sqr(cx - x2) + sqr(cy - y2);
+	dd3 = sqr(cx - x3) + sqr(cy - y3);
+	dd4 = sqr(cx - x4) + sqr(cy - y4);
+	return dd1 == dd2 && dd1 == dd3 && dd1 == dd4;
+}
