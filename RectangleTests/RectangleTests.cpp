4c492a22fbcb579df7ba012a10fbb225e190eb54
Yeshd <yeshodaprasai@gmail.com>
2023-04-11 15:06:25 -0400

    porper test case

--- RectangleTests/RectangleTests.cpp
+++ RectangleTests/RectangleTests.cpp
@@ -3,12 +3,13 @@
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
-namespace RectangleUnitTest
+namespace RectangleTests
 {
-	TEST_CLASS(RectangleUnitTest)
+	TEST_CLASS(RectangleTests)
 	{
-	public:  
+	public:
 		
+
 		TEST_METHOD(TestMethod1)
 		{
 			int x1 = 2, y1 = 5, x2 = 8, y2 = 5, x3 = 8, y3 = 3, x4 = 2, y4 = 3;
@@ -59,7 +60,7 @@ namespace RectangleUnitTest
 			int a = (x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1); 
 			int b = (x3 - x2) * (x3 - x2) + (y3 - y2) * (y3 - y2); 
 			int c = (x4 - x3) * (x4 - x3) + (y4 - y3) * (y4 - y3); 
-			int d = (x1 - x4) * (x1 - x4) + (y1 - y4) * (y1 - y4); 
+			int d = (x1 - x4) * (x1 - x4) + (y1 - y4) * (y1 - y4);
 
 			area = a * b;
 			Assert::AreEqual(11700, area);
@@ -94,7 +95,5 @@ namespace RectangleUnitTest
 
 		}
 	};
-}
 
-
-
+	};
